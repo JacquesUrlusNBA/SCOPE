118ec67dff5979f53162df3156764bf8ef67f3e1
Jacques Urlus <jurlus@zonnet.nl>
2026-02-09 11:26:19 +0100

    Add project files.

---
+++ SCOPE/FrmMain.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmMain))
+        Me.CmdCreate = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.CmdClose = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CmdCreate
+        '
+        Me.CmdCreate.Location = New System.Drawing.Point(619, 415)
+        Me.CmdCreate.Name = "CmdCreate"
+        Me.CmdCreate.Size = New System.Drawing.Size(75, 23)
+        Me.CmdCreate.TabIndex = 0
+        Me.CmdCreate.Text = "Create"
+        Me.CmdCreate.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(776, 242)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 302)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'CmdClose
+        '
+        Me.CmdClose.Location = New System.Drawing.Point(713, 415)
+        Me.CmdClose.Name = "CmdClose"
+        Me.CmdClose.Size = New System.Drawing.Size(75, 23)
+        Me.CmdClose.TabIndex = 3
+        Me.CmdClose.Text = "Close"
+        Me.CmdClose.UseVisualStyleBackColor = True
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 486)
+        Me.Controls.Add(Me.CmdClose)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.CmdCreate)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "FrmMain"
+        Me.Text = "SBR Conversion for Other GAAP Package"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CmdCreate As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents CmdClose As Button
+End Class
---
+++ SCOPE/FrmMain.vb
@@ -0,0 +1,7 @@
+﻿Public Class FrmMain
+    Private Sub CmdClose_Click(sender As Object, e As EventArgs) Handles CmdClose.Click
+
+        Me.Close()
+
+    End Sub
+End Class
